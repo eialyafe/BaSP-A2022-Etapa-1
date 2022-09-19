b60303e3a6a5c952d8d4f15883c8f666bbd18e5b
Eial Alalachvily <eialyafe@gmail.com>
2022-09-18 21:08:15 -0300

    BaSP-W05-2022: folder05 created and excercise 1 related to variables and operators

---
+++ Week-05/excercises/01_variables_and_operators.js
@@ -0,0 +1,31 @@
+console.log ('EXCERCISE 1: VARIABLES AND OPERATORS');
+
+// a. Crear dos variables numéricas y utilizar el operador suma para guardar el
+// valor de la suma de ambos números en una 3er variable.
+console.log ('EXCERCISE 1.a');
+
+var ageMarcos = 3;
+var ageAndres = 2;
+var agesAddition = ageAndres + ageMarcos;
+
+console.log ('Addition ages is ',agesAddition);
+
+// b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+console.log ('EXCERCISE 1.b')
+
+var firstName =  'Henry';
+var secondName =  'Gabriel';
+var fullName = firstName + secondName;
+
+console.log ('Fullname ',fullName);
+
+// c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+// guardando el resultado de la suma en una 3er variable (utilizar length).
+console.log ('EXCERCISE 1.c');
+
+var names = 'Rodrigo Henry';
+var surnames = 'Gonzales Martinez';
+var lettersCant = (names,surnames);
+
+console.log ('Names and surnames contains ',lettersCant.length, ' letters');
+
